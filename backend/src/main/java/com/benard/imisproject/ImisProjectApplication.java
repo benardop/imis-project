86fc5c1602394bd117c0eb45d4ba95a1881711c3
benardop <benard.oc.p@gmail.com>
2023-09-02 21:12:37 +0300

    Add project files

---
+++ backend/src/main/java/com/benard/imisproject/ImisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.benard.imisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImisProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/benard/imisproject/ImisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benard.imisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
